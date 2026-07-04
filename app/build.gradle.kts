74537e2da130c4cd308246a7e2e173109fae9cb7
antinormies <antiableofnormiesable@gmail.com>
2026-07-04 07:09:18 +0700

    initial

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,16 +4,12 @@ plugins {
 
 android {
     namespace = "id.my.daniza.pixheal"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "id.my.daniza.pixheal"
         minSdk = 27
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
